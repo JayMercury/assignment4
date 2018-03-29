c6cea842129f4fc934f5c82b8dc339cd42e695b9
Christopher Ng <christopherng1990@gmail.com>
2018-03-29 18:06:24 -0400

    finished first part

--- code/A4_2.m
+++ code/A4_2.m
@@ -47,11 +47,13 @@ Foff = [Vin-Vin;
         0;
         0;
         0;];
+
+ts = 1000;              % Time step
     
-V1 = zeros(7, 1000);
+V1 = zeros(7, ts);
 Vstart = zeros(7, 1);
 dt = 1e-3;
-for i = 1:1000
+for i = 1:ts
 
     if i < 30
         V1(:,i) = (C./dt+G)\(Foff+C*Vstart/dt);
@@ -64,18 +66,19 @@ for i = 1:1000
     
 end
 figure(1)
-plot(1:1000, V1(7,:))
+plot(1:ts, V1(7,:), 'r')
 hold on
-plot(1:1000, V1(1,:))
+plot(1:ts, V1(1,:), 'b')
 title('Vin and Vout with a step at 0.03s')
 xlabel('Time (ms)')
 ylabel('Voltage (V)')
+grid on
 
-V2 = zeros(7, 1000);
+V2 = zeros(7, ts);
 Fsin = zeros(7,1);
-for j = 1:1000
+for j = 1:ts
 
-    Vsin = sin(2*pi*(1/0.03)*j/1000);
+    Vsin = sin(2*pi*(1/0.03)*j/ts);
     Fsin(1,1) = Vsin;
     if j == 1
         V2(:,j) = (C./dt+G)\(Fsin+C*Vstart/dt);
@@ -86,18 +89,19 @@ for j = 1:1000
         
 end
 figure(2)
-plot(1:1000, V2(7,:))
+plot(1:ts, V2(7,:), 'r')
 hold on
-plot(1:1000, V2(1,:))
+plot(1:ts, V2(1,:), 'b')
 title('Vin and Vout with sin function of f = 1/0.03')
 xlabel('Time (ms)')
 ylabel('Voltage (V)')
+grid on
 
-V3 = zeros(7, 1000);
-Fguass = zeros(7,1);
-for k = 1:1000
+V3 = zeros(7, ts);
+Fgauss = zeros(7,1);
+for k = 1:ts
 
-    Vgauss = exp(-1/2*((k/1000-0.06)/(0.03))^2);
+    Vgauss = exp(-1/2*((k/ts-0.06)/(0.03))^2);
     Fgauss(1,1) = Vgauss;
     if k == 1
         V3(:,k) = (C./dt+G)\(Fgauss+C*Vstart/dt);
@@ -108,6 +112,48 @@ for k = 1:1000
         
 end
 figure(3)
-plot(1:1000, V3(7,:))
+plot(0:ts-1, V3(7,:), 'r')
 hold on
-plot(1:1000, V3(1,:))
+plot(0:ts-1, V3(1,:), 'b')
+title('Vin and Vout with gaussian function')
+xlabel('Time (ms)')
+ylabel('Voltage (V)')
+grid on
+
+f = (0:ts-1);               % Frequency range
+
+fV1in = fft(V1(1, :));
+fV1out = fft(V1(7, :));
+fsV1in = fftshift(fV1in);
+fsV1out = fftshift(fV1out);
+figure(4)
+plot(f, abs(fsV1in), 'r')
+hold on
+plot(f, abs(fsV1out), 'b')
+title('Vin and Vout in frequency domain with a step function')
+xlabel('frequency (1/ms)')
+ylabel('Voltage (V)')
+grid on
+
+fV2 = fft(V2.');
+fsV2 = fftshift(fV2);
+figure(5)
+plot(f, abs(fsV2(:, 1)), 'r')
+hold on
+plot(f, abs(fsV2(:, 7)), 'b')
+title('Vin and Vout in frequency domain with a sin function')
+xlabel('frequency (1/ms)')
+ylabel('Voltage (V)')
+grid on
+
+fV3 = fft(V3.');
+fsV3 = fftshift(fV3);
+figure(6)
+plot(f, abs(fsV3(:, 1)), 'r')
+hold on
+plot(f, abs(fsV3(:, 7)), 'b')
+title('Vin and Vout in frequency domain with a gaussian function')
+xlabel('frequency (1/ms)')
+ylabel('Voltage (V)')
+grid on
+
